caff0ae79ea14e76d5973bca1fb2072dc240c2e1
Monni-Tonon <monnicy@gmail.com>
2023-04-24 15:11:38 -0300

    config com banco e base das rotas concluida

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const produtos = require("./routes/produtos");
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL)
+
+// chamar rotas
+app.use("/produtos", produtos);
+
+app.listen(3000, () => {
+    console.log("Aplicação rodando em http://localhost:3000")
+})
